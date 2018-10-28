511f5435ea4c5c1be993221fdcf300279be5c5b7
rcarrette <romain.carrette@live.fr>
2018-10-28 13:02:20 +0100

    updated style

--- angular-socketio-stock-dashboard/src/app/app.module.ts
+++ angular-socketio-stock-dashboard/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './components/app.component';
