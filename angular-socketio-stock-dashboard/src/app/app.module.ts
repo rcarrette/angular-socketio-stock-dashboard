b91ea5042d824cb1ec9bfa0df8e30ac0fc5c0b11
rcarrette <romain.carrette@live.fr>
2018-10-28 12:24:29 +0100

    updated style and added headline component

--- angular-socketio-stock-dashboard/src/app/app.module.ts
+++ angular-socketio-stock-dashboard/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './components/app.component';
+import { HeadlineComponent } from './components/headline/headline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadlineComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-socketio-stock-dashboard/src/app/components/headline/headline.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headline',
+  templateUrl: './headline.component.html',
+  styleUrls: ['./headline.component.less']
+})
+export class HeadlineComponent implements OnInit {
+  title = 'Live stock dashboard';
+  subtitle = 'powered by IEX Ⓒ';
+  subtitleHref = 'https://iextrading.com';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
