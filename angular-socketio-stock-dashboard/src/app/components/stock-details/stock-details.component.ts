e89d5eba5822e7c0b80c21946f22360703b81fbc
rcarrette <romain.carrette@live.fr>
2018-12-12 18:04:19 +0100

    improved chart style and socket performance

--- angular-socketio-stock-dashboard/src/app/components/stock-details/stock-details.component.ts
+++ angular-socketio-stock-dashboard/src/app/components/stock-details/stock-details.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, OnChanges } from '@angular/core'
+import { Component, Input, OnChanges, OnDestroy } from '@angular/core'
 import { Chart } from 'angular-highcharts'
+import { Subscription } from 'rxjs'
+
+import * as moment from 'moment'
 
 import { StockService } from 'src/app/services/stock.service'
-
 import { Stock } from '../../models/stock'
 
 @Component({
@@ -10,9 +12,10 @@ import { Stock } from '../../models/stock'
   templateUrl: './stock-details.component.html',
   styleUrls: ['./stock-details.component.less']
 })
-export class StockDetailsComponent implements OnChanges {
+export class StockDetailsComponent implements OnChanges, OnDestroy {
   @Input() stock: Stock
-  chart: any;
+  chart: any
+  stockSubscription: Subscription
 
   constructor(private stockService: StockService) { }
 
@@ -20,36 +23,76 @@ export class StockDetailsComponent implements OnChanges {
     this.initChart()
   }
 
+  ngOnDestroy() {
+    if (this.stockSubscription)
+      this.stockSubscription.unsubscribe()
+  }
+
   initChart(): void {
     if (this.stock) {
       let chart = new Chart({
         chart: {
-          type: 'line'
-          // backgroundColor: '#000000'
-        },
-        title: {
-          text: ''
-        },
-        credits: {
-          enabled: false
+          type: 'line',
+          backgroundColor: '#1a1a1a'
         },
         series: [{
           name: this.stock.symbol,
           data: this.stock.pricesHistory.map(p => p.value)
-        }]
+        }],
+        xAxis: {
+          categories: this.stock.pricesHistory.map(p => this.dateToFormattedString(p.date))
+        },
+        yAxis: {
+          title: {
+            text: 'Stock price'
+          },
+          gridLineColor: '#262626',
+          labels: {
+            format: '${value}'
+          }
+        },
+        plotOptions: {
+          series: {
+            dataLabels: {
+              format: '${y}',
+              color: '#ffffff',
+              enabled: true
+            }
+          }
+        },
+        title: {
+          text: ''
+        },
+        legend: {
+          enabled: false
+        },
+        tooltip: {
+          useHTML: true,
+          formatter: function () {
+            return '<b>$' + this.y + '</b> <small>' + this.x + '</small>'
+          }
+        },
+        credits: {
+          enabled: false
+        }
       })
 
       this.chart = chart
 
-      //TODO use Rxjs multicasting instead (to allow all subscribers to get the same value at the same time)
-      this.stockService.onStockUpdate().subscribe((stock: Stock) => this.addPoint(stock))
+      this.stockSubscription = this.stockService.onStockUpdate().subscribe((stock: Stock) => this.addPoint(stock))
     }
   }
 
   addPoint(stock: Stock): void {
     const isEqualToLastValue = (arr, value) => arr[arr.length - 1].y == value //TODO use lodash or ramda every time I manipulate an array
 
-    if (stock.symbol == this.stock.symbol && !isEqualToLastValue(this.chart.ref$.source.value.series[0].data, stock.currentPrice.value))
+    if (stock.symbol == this.stock.symbol && !isEqualToLastValue(this.chart.ref$.source.value.series[0].data, stock.currentPrice.value)) {
+      this.chart.ref$.source.value.xAxis[0].categories.push(this.dateToFormattedString(stock.currentPrice.date))
       this.chart.addPoint(stock.currentPrice.value)
+    }
+  }
+
+  dateToFormattedString(date: Date): string {
+    return moment(date).format('MM/DD/YYYY HH:mm:ss')
   }
 }
